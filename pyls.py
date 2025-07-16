d49aa388e0ea45ac0f17549c558aa446699a255b
babayaga-09 <aryangrang@gmail.com>
2025-07-16 12:00:57 +0530

    Update pyls.py

--- pyls.py
+++ pyls.py
@@ -2,88 +2,80 @@
 # this file, which is itself a "module".
 import argparse
 import os
-
+import datetime
 
 def main() -> None:
     parser = argparse.ArgumentParser(prog="pyls", description="A baby version of ls.")
-
     # We now add descriptions for the expected arguments for our program.
     parser.add_argument(
         "dirname",
-        nargs="?",  # Indicates that either 0 or one directory name can be given.
-        default=".",  # Gives the default value to use when this argument is not given.
-        # '.' means "current directory".
+        nargs="?",# Indicates that either 0 or one directory name can be given.
+        default=".", # Gives the default value to use when this argument is not given.
+         # '.' means "current directory".
         help="The name of the directory whose contents are to be listed.",
     )
     parser.add_argument(
-        "-l",
-        "--longform",
-        action="store_true",  # Indicates that the value is a boolean and no further
-        # values need to be supplied on the command line.
-        default=False,  # This is not really needed as False is the default already
+        "-l", "--longform",
+        action="store_true",# Indicates that the value is a boolean and no further
+           # values need to be supplied on the command line.
+        default=False,# This is not really needed as False is the default already
         # when -l is not given on the command line. Including it
         # for illustration.
-        help="Prints details about each file in the format -\n"
-        "<timestamp> <filesize> <filename>",
+        help="Prints details about each file: <timestamp> <filesize> <filename>",
     )
-    parser.add_argument(
-        "-F",
-        "--formatted",
-        action="store_true",
-        default=False,
-        help="Adds a character to tell you whether its a file or a directory.",
-    )
-
-    # Now we ask argparse to use the above information to interpret the
+      # Now we ask argparse to use the above information to interpret the
     # arguments and give us an object which will have `.dirname`,
     # `.longform` and `/.formatted` attributes we can access instead of
     # having to check for all the combinations ourselves.
     # Furthermore, if you call your program with either the `-h` or `--help``
     # flag, it will print out all of the specification above in a nice format
     # as help. This is a convention employed by most command line programs.
+    parser.add_argument(
+        "-F", "--formatted",
+        action="store_true",
+        default=False,
+        help="Adds / for directories, * for executables.",
+    )
+
     args = parser.parse_args()
+    
+    # Call the actual logic function
+    pyls(args.dirname, args.longform, args.formatted)
 
 
 def pyls(dirname: str, longform: bool, formatted: bool) -> None:
-    """
-    DATA REPRESENTATION
-    -------------------
+    try:
+        entries = os.listdir(dirname)
+    except FileNotFoundError:
+        print(f"Error: Directory '{dirname}' not found.")
+        return
+    except NotADirectoryError:
+        print(f"Error: '{dirname}' is not a directory.")
+        return
 
-    In this case, we're choosing a **representation** where we represent
-    the directory name to list as a string and the choice of longform and
-    formatted output as two boolean values.
-   
-    SIGNATURE 
-    ---------
-    The "signature" of the procedure below can be written as
-              str bool bool -> None
+    for name in sorted(entries):
+        full_path = os.path.join(dirname, name)
+        display_name = name
 
-    PURPOSE
-    -------
+        # Add / or * if formatted
+        if formatted:
+            if os.path.isdir(full_path):
+                display_name += "/"
+            elif os.access(full_path, os.X_OK):
+                display_name += "*"
 
-    TODO: Replace this content with a sentence or two describing what this
-    function does.
-    - :param dirname: TODO: Description of `dirname` parameter.
-    - :param longform: TODO: Description of `longform` parameter.
-    - :param formatted: TODO: Description of `formatted` parameter.
-
-    EXAMPLES
-    --------
-
-    TODO: Below, give a few examples of what you expect the procedure to do when you
-    give various inputs. This can help you think about what to implement.
-    Consider various possible combinations.
-    """
-    # Replace the "pass" below with your implementation.
-    pass
+        # Print in long form
+        if longform:
+            try:
+                stats = os.stat(full_path)
+                size = stats.st_size
+                timestamp = datetime.datetime.fromtimestamp(stats.st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+                print(f"{timestamp} {size:>8} {display_name}")
+            except Exception as e:
+                print(f"Error reading file info: {e}")
+        else:
+            print(display_name)
 
 
-# A python module may be loaded in one of two ways --
-# 1. `python myfile.py`: In this case, the python file/module is considered to be
-#    a "main program" and is named "__main__" within. So if you want to run code
-#    only in this mode, you check whether __name__ is "__main__" and run that code.
-# 2. `import myfile`: (within some other python file). In this mode, it is a pure
-#    "module" that exposes functions and values via `myfile.` notation within the
-#    importing python file. In this case, __name__ will be "myfile" and not "__main__".
 if __name__ == "__main__":
     main()
